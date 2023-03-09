eff295fe0c0c5559a55ab76e0270615811c18483
dreamy-timmy <120133207+dreamy-timmy@users.noreply.github.com>
2023-03-09 19:45:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -67,20 +67,61 @@
 #     if tops == I: break
 # print(sm)
 
+# Дейкстра
+I = [1,2,3,4,5,6,7,8,9,10,11,12]
+ribs = [[1,2], [1,5], [1,6], [2,3], [2,4], [2,5], [2,6], [3,4],
+        [3,5], [4,5], [4,7], [4,8], [5,6], [5,7], [5,8], [5,9],
+        [6,8], [6,9], [7,8],[7,10], [7,11], [8,9], [8,10], [8,11], [8,12],
+        [9,11], [9,12], [10,11], [11,12]]
+weight = [7, 9, 2, 5, 4, 8, 2, 2, 9, 3, 8, 3, 3, 5, 1, 7, 6, 1, 6, 4, 4, 2, 7, 8, 5, 6, 1, 10, 3]
+tops = set()
+sm = 0
+s = [None for i in range(len(I))]
+tops.add(I[0])
+s[I[0]-1] = 0
+current = []
+top = 1
+ind = -1
+delete = []
+f = 1
+while ribs:
+        if ind + 1 <= len(ribs)-1: ind += 1
+        else:
+            if current:
+                mn = s[current[0] - 1]
+                t = current[0]
+                for j in range(len(current)):
+                    if mn > s[current[j] - 1]:
+                        t = current[j]
+                        mn = s[current[j] - 1]
+                tops.add(t)
+                top = t
+                current = []
+                ind = 0
+                delete.sort(reverse=1)
+                while delete:
+                    ribs.pop(delete[0])
+                    weight.pop(delete[0])
+                    delete.pop(0)
+            else:
+                for i in range(len(I)):
+                    if I[i] not in tops:
+                        top = I[i]
+                        tops.add(top)
+                        break
+        if weight and top == ribs[ind][0] and f:
+            if s[ribs[ind][1] - 1]:
+                    s[ribs[ind][1]-1] = min(s[ribs[ind][0]-1] + weight[ind], s[ribs[ind][1]-1])
+            else:
+                    s[ribs[ind][1]-1] = s[ribs[ind][0]-1] + weight[ind]
+            current.append(ribs[ind][1])
+            delete.append(ind)
+        elif weight and top == ribs[ind][1] and f:
+            if s[ribs[ind][0] - 1]:
+                    s[ribs[ind][0]-1] = min(s[ribs[ind][1]-1] + weight[ind], s[ribs[ind][0]-1])
+            else:
+                    s[ribs[ind][0]-1] = s[ribs[ind][1]-1] + weight[ind]
+            current.append(ribs[ind][0])
+            delete.append(ind)
+print(s)
 
-# while weight:
-#     ind = weight.index(min(weight))
-#     if ribs[ind][0] in tops or ribs[ind][0] in tops: ... #sall good
-#     elif ribs[ind][0] in tops and ribs[ind][0] in tops:
-#         weight.remove(weight[ind])
-#         ribs.remove(ribs[ind])
-#     else:
-#         current = [i for i in weight]
-#         ind = current.index(min(current))
-#         while not(ribs[ind][0] in tops or ribs[ind][0] in tops):
-#             current = [weight[i] for i in range(len(weight)) if i != current.index(min(current))]
-#     tops.add(ribs[ind][0])
-#     tops.add(ribs[ind][1])
-#     sm += weight[ind]
-#     weight.remove(weight[ind])
-#     ribs.remove(ribs[ind])
